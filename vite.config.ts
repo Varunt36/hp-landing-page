529c6cc85578a4d22108a8c9b52556ffec7b20e7
varun thaker <vthakar3@gmail.com>
2026-04-26 21:46:27 +0200

    updated vite config lesser chunk and added alibe for supabase

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-router')) return 'router'
+            if (id.includes('react') || id.includes('scheduler')) return 'react-vendor'
+            if (id.includes('@supabase')) return 'supabase'
+            if (id.includes('@mui') || id.includes('@emotion')) return 'mui'
+            if (id.includes('@fontsource')) return 'fonts'
+            if (id.includes('html5-qrcode')) return 'qrcode'
+            if (id.includes('zustand')) return 'state'
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
